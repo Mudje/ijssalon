74d9212a355cd83ded8e4a056b1eb463a0ab4509
Janssen <martijn.janssen@vodafoneziggo.com>
2025-02-19 15:42:27 +0100

    Dit is de eeerste versie

---
+++ Opdracht 1.py
@@ -0,0 +1,12 @@
+naam = input("Wat is je naam? ")
+geboorteplaats = input("Wat is je geboorteplaats? ")
+leeftijd = int(input("Wat is je leeftijd? "))
+print("Naam: " + naam)
+print("Geboorteplaats: "+ geboorteplaats)
+if leeftijd > 50:
+    print("Leeftijd: " + str(leeftijd))
+    print("Heb je al een rollator aangeschaft?")
+else:
+    print("Leeftijd: " + str(leeftijd))
+
+    
